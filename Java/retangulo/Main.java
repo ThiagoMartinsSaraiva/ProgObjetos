4a2a30764df5991438ee854e8522653fd285ca9e
Thiago Martins Saraiva <thiagomartinssaraiva@gmail.com>
2019-04-03 23:41:22 -0300

    added the packages in the ratangulo

---
+++ Java/retangulo/Main.java
@@ -0,0 +1,21 @@
+package retangulo;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Retangulo r = new Retangulo();
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter the width: ");
+    float width = scan.nextFloat();
+    r.setWidth(width); 
+    System.out.println("Enter the height: ");
+    float height = scan.nextFloat();
+    r.setHeight(height);
+    System.out.println("Width is: " + r.getWidth());
+    System.out.println("Height is: " + r.getHeight());
+    System.out.println("The area is: " + r.calculateArea(r.getWidth(), r.getHeight()));
+    System.out.println("The perimeter is: " + r.calculatePerimeter(r.getWidth(), r.getHeight()));
+    scan.close();
+  }
+}
---
+++ Java/retangulo/Retangulo.java
@@ -0,0 +1,40 @@
+package retangulo;
+
+public class Retangulo {
+  private float width;
+  private float height;
+
+  public float getWidth() {
+    return width;
+  }
+
+  public void setWidth(float width) {
+    this.width = width;
+  }
+
+  public float getHeight() {
+    return height;
+  }
+
+  public void setHeight(float height) {
+    this.height = height;
+  } 
+
+  public Retangulo(float width, float height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public Retangulo() {
+    
+  }
+
+  public float calculateArea(float width, float height) {
+    return width * height;
+  }
+
+  public float calculatePerimeter(float width, float height) {
+    return width * 2 + height * 2;
+  }
+  
+}
